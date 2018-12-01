11d7b87200915ec386a04c432990883e5815b185
Z. Wang <11609546+ZuowenWang0000@users.noreply.github.com>
2018-12-01 16:56:05 +0800

    Add files via upload

---
+++ Assignment3/Project1/Project1/Source.cpp
@@ -0,0 +1,476 @@
+/*********************************************************
+FILE : main.cpp (csci3260 2018-2019 Project)
+*********************************************************/
+/*********************************************************
+Student Information
+Student ID: 1155123906	
+Student Name: Zuowen Wang
+
+Student ID: 1155123308
+Student Name: Wentao Zhu
+*********************************************************/
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include "C:\Users\cprj2748\Desktop\CG\Rotating-energy-rings-master\Assignment3\Project1\Project1\Dependencies\glew\glew.h"
+#include "C:\Users\cprj2748\Desktop\CG\Rotating-energy-rings-master\Assignment3\Project1\Project1\Dependencies\freeglut\freeglut.h"
+#include "C:\Users\cprj2748\Desktop\CG\Rotating-energy-rings-master\Assignment3\Project1\Project1\Dependencies\glm\glm.hpp"
+#include "C:\Users\cprj2748\Desktop\CG\Rotating-energy-rings-master\Assignment3\Project1\Project1\Dependencies\glm\gtc\matrix_transform.hpp"
+#include "C:\Users\cprj2748\Desktop\CG\Rotating-energy-rings-master\Assignment3\Project1\Project1\Dependencies\glm\gtc\type_ptr.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "camera.h"
+
+GLuint loadBMP_custom(const char * imagepath);
+unsigned char* loadBMP_data(const GLchar* imagepath, int* width, int* height);
+unsigned int loadCubemap(vector<const GLchar*> faces);
+
+using namespace std;
+using glm::vec3;
+using glm::mat4;
+
+Camera* cam;
+
+
+int WIDTH = 1920;
+int HEIGHT = 1080;
+
+GLint programID;
+GLint skyboxProgramID;
+// Could define the Vao&Vbo and interaction parameter here
+
+
+//a series utilities for setting shader parameters 
+void setMat4(const std::string &name, glm::mat4& value)
+{
+	unsigned int transformLoc = glGetUniformLocation(programID, name.c_str());
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void setVec4(const std::string &name, glm::vec4 value)
+{
+	glUniform4fv(glGetUniformLocation(programID, name.c_str()), 1, &value[0]);
+}
+void setVec3(const std::string &name, glm::vec3 value)
+{
+	glUniform3fv(glGetUniformLocation(programID, name.c_str()), 1, &value[0]);
+}
+void setFloat(const std::string &name, float value)
+{
+	glUniform1f(glGetUniformLocation(programID, name.c_str()), value);
+}
+void setInt(const std::string &name, int value)
+{
+	glUniform1i(glGetUniformLocation(programID, name.c_str()), value);
+}
+
+bool checkStatus(
+	GLuint objectID,
+	PFNGLGETSHADERIVPROC objectPropertyGetterFunc,
+	PFNGLGETSHADERINFOLOGPROC getInfoLogFunc,
+	GLenum statusType)
+{
+	GLint status;
+	objectPropertyGetterFunc(objectID, statusType, &status);
+	if (status != GL_TRUE)
+	{
+		GLint infoLogLength;
+		objectPropertyGetterFunc(objectID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* buffer = new GLchar[infoLogLength];
+
+		GLsizei bufferSize;
+		getInfoLogFunc(objectID, infoLogLength, &bufferSize, buffer);
+		cout << buffer << endl;
+
+		delete[] buffer;
+		return false;
+	}
+	return true;
+}
+
+bool checkShaderStatus(GLuint shaderID)
+{
+	return checkStatus(shaderID, glGetShaderiv, glGetShaderInfoLog, GL_COMPILE_STATUS);
+}
+
+bool checkProgramStatus(GLuint programID)
+{
+	return checkStatus(programID, glGetProgramiv, glGetProgramInfoLog, GL_LINK_STATUS);
+}
+
+string readShaderCode(const char* fileName)
+{
+	ifstream meInput(fileName);
+	if (!meInput.good())
+	{
+		cout << "File failed to load..." << fileName;
+		exit(1);
+	}
+	return std::string(
+		std::istreambuf_iterator<char>(meInput),
+		std::istreambuf_iterator<char>()
+	);
+}
+
+void installShaders()
+{
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const GLchar* adapter[1];
+	string temp = readShaderCode("VertexShaderCode.glsl");
+	adapter[0] = temp.c_str();
+	glShaderSource(vertexShaderID, 1, adapter, 0);
+	temp = readShaderCode("FragmentShaderCode.glsl");
+	adapter[0] = temp.c_str();
+	glShaderSource(fragmentShaderID, 1, adapter, 0);
+
+	glCompileShader(vertexShaderID);
+	glCompileShader(fragmentShaderID);
+
+	if (!checkShaderStatus(vertexShaderID) || !checkShaderStatus(fragmentShaderID))
+		return;
+
+	programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+
+	if (!checkProgramStatus(programID))
+		return;
+
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
+
+	glUseProgram(programID);
+}
+
+void installSkyboxShaders()
+{
+	GLuint skyboxVertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint skyboxFragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const GLchar* adapter[1];
+	string temp = readShaderCode("skyboxvert.glsl");
+	adapter[0] = temp.c_str();
+	glShaderSource(skyboxVertexShaderID, 1, adapter, 0);
+	temp = readShaderCode("skyboxfrag.glsl");
+	adapter[0] = temp.c_str();
+	glShaderSource(skyboxFragmentShaderID, 1, adapter, 0);
+
+	glCompileShader(skyboxVertexShaderID);
+	glCompileShader(skyboxFragmentShaderID);
+
+	if (!checkShaderStatus(skyboxVertexShaderID) || !checkShaderStatus(skyboxFragmentShaderID))
+		return;
+
+	skyboxProgramID = glCreateProgram();
+	glAttachShader(skyboxProgramID, skyboxVertexShaderID);
+	glAttachShader(skyboxProgramID, skyboxFragmentShaderID);
+	glLinkProgram(skyboxProgramID);
+
+	if (!checkProgramStatus(skyboxProgramID))
+		return;
+
+	glDeleteShader(skyboxVertexShaderID);
+	glDeleteShader(skyboxFragmentShaderID);
+
+	glUseProgram(skyboxProgramID);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	//TODO: Use keyboard to do interactive events and animation
+
+}
+
+void move(int key, int x, int y)
+{
+	//TODO: Use arrow keys to do interactive events and animation
+}
+
+void PassiveMouse(int x, int y)
+{
+	//TODO: Use Mouse to do interactive events and animation
+
+}
+
+bool loadOBJ(const char * path,std::vector<glm::vec3> & out_vertices,std::vector<glm::vec2> & out_uvs,std::vector<glm::vec3> & out_normals) {
+	printf("Loading OBJ file %s...\n", path);
+
+	std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+	std::vector<glm::vec3> temp_vertices;
+	std::vector<glm::vec2> temp_uvs;
+	std::vector<glm::vec3> temp_normals;
+
+
+	FILE * file = fopen(path, "r");
+	if (file == NULL) {
+		printf("Impossible to open the file ! Are you in the right path ? See Tutorial 6 for details\n");
+		getchar();
+		return false;
+	}
+
+	while (1) {
+
+		char lineHeader[128];
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
+
+				   // else : parse lineHeader
+
+		if (strcmp(lineHeader, "v") == 0) {
+			glm::vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			temp_vertices.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			glm::vec2 uv;
+			fscanf(file, "%f %f\n", &uv.x, &uv.y);
+			uv.y = -uv.y;
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			glm::vec3 normal;
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			if (matches != 9) {
+				printf("File can't be read by our simple parser :-( Try exporting with other options\n");
+				return false;
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			uvIndices.push_back(uvIndex[0]);
+			uvIndices.push_back(uvIndex[1]);
+			uvIndices.push_back(uvIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
+		}
+		else {
+			char stupidBuffer[1000];
+			fgets(stupidBuffer, 1000, file);
+		}
+
+	}
+
+	// For each vertex of each triangle
+	for (unsigned int i = 0; i<vertexIndices.size(); i++) {
+
+		// Get the indices of its attributes
+		unsigned int vertexIndex = vertexIndices[i];
+		unsigned int uvIndex = uvIndices[i];
+		unsigned int normalIndex = normalIndices[i];
+
+		// Get the attributes thanks to the index
+		glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+		glm::vec2 uv = temp_uvs[uvIndex - 1];
+		glm::vec3 normal = temp_normals[normalIndex - 1];
+
+		// Put the attributes in buffers
+		out_vertices.push_back(vertex);
+		out_uvs.push_back(uv);
+		out_normals.push_back(normal);
+
+	}
+
+	return true;
+}
+GLuint loadBMP_custom(const char * imagepath) {
+
+	printf("Reading image %s\n", imagepath);
+
+	unsigned char header[54];
+	unsigned int dataPos;
+	unsigned int imageSize;
+	unsigned int width, height;
+	unsigned char * data;
+
+	FILE * file = fopen(imagepath, "rb");
+	if (!file) { printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar(); return 0; }
+
+	if (fread(header, 1, 54, file) != 54) {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	if (*(int*)&(header[0x1E]) != 0) { printf("Not a correct BMP file\n");    return 0; }
+	if (*(int*)&(header[0x1C]) != 24) { printf("Not a correct BMP file\n");    return 0; }
+
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+	if (imageSize == 0)    imageSize = width * height * 3;
+	if (dataPos == 0)      dataPos = 54;
+
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+
+
+	GLuint textureID;
+	//TODO: Create one OpenGL texture and set the texture parameter 
+	//clarification: for this part we simply copied from the tutorial notes
+
+	glGenTextures(1, &textureID);
+
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	// Give the image to OpenGL
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+
+
+	// OpenGL has now copied the data. Free our own version
+	delete[] data;
+	return textureID;
+}
+unsigned int loadCubemap(vector<const GLchar*> faces)
+{
+	int* width = (int *)malloc(sizeof(int *));
+	int* height = (int *)malloc(sizeof(int *));
+	*width = 0;
+	*height = 0;
+	unsigned char* imagedata;
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+	for (GLuint i = 0; i < faces.size(); i++) {
+
+		imagedata = loadBMP_data(faces[i], width, height);
+
+		int temp_width = *width; int temp_height = *height;
+
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, temp_width, temp_height,
+			0, GL_BGR, GL_UNSIGNED_BYTE, imagedata);
+
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	//glBindTexture(GL_TEXTURE_CUBE_MAP, -1);
+	return textureID;
+}
+unsigned char* loadBMP_data(const GLchar* imagepath, int* width, int* height) {
+
+	printf("Reading image %s\n", imagepath);
+
+	unsigned char header[54];
+	unsigned int dataPos;
+	unsigned int imageSize;
+	//unsigned int width, height;
+	unsigned char * data;
+
+	FILE * file = fopen((char*)imagepath, "rb");
+	if (!file) { printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar(); return 0; }
+
+	if (fread(header, 1, 54, file) != 54) {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	if (*(int*)&(header[0x1E]) != 0) { printf("Not a correct BMP file\n");    return 0; }
+	if (*(int*)&(header[0x1C]) != 24) { printf("Not a correct BMP file\n");    return 0; }
+
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	*width = *(int*)&(header[0x12]);
+	*height = *(int*)&(header[0x16]);
+	if (imageSize == 0)    imageSize = (*width) * (*height * 3);
+	if (dataPos == 0)      dataPos = 54;
+
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+
+	// OpenGL has now copied the data. Free our own version
+
+	return data;
+}
+
+void sendDataToOpenGL()
+{
+	//TODO:
+	//Load objects and bind to VAO & VBO
+	//Load texture
+
+
+}
+
+void paintGL(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//TODO:
+	//Set lighting information, such as position and color of lighting source
+	//Set transformation matrix
+	//Bind different textures
+	
+
+
+
+
+
+
+
+	glFlush();
+	glutPostRedisplay();
+}
+
+void initializedGL(void) //run only once
+{
+	glewInit();
+	installShaders();
+	sendDataToOpenGL();
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Assignment 2. openGL is so difficult");
+
+
+	//TODO:
+	/*Register different CALLBACK function for GLUT to response
+	with different events, e.g. window sizing, mouse click or
+	keyboard stroke */
+	initializedGL();
+
+	glutDisplayFunc(paintGL);
+
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(move);
+	glutPassiveMotionFunc(PassiveMouse);
+
+
+	glutMainLoop();
+
+	return 0;
+}
+
