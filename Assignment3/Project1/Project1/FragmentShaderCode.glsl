54603a9439524ad7569642ffab604dc35c35f2f9
Z. Wang <11609546+ZuowenWang0000@users.noreply.github.com>
2018-12-02 12:44:17 +0800

    Add files via upload

--- Assignment3/Project1/Project1/FragmentShaderCode.glsl
+++ Assignment3/Project1/Project1/FragmentShaderCode.glsl
@@ -26,43 +26,37 @@ uniform vec3 specularStrength;
 void main()
 {
 	vec3 normal = normalize(normalWorld);
-	//if(normalMapping_flag){
+	if(normalMapping_flag){
 		normal = texture(myTextureSampler_1, UV).rgb;
 		normal = normalize(normal*2 - 1.0);
-	//}
+		daColor = vec4((normal)*(ambientLight), 1.0);
+	}else{
+
+		float constant = 1f;
+		float linear = 0.0035f;
+		float quadratic = 0.0044f;
 
 
-
-	float constant = 1f;
-	float linear = 0.0035f;
-	float quadratic = 0.0044f;
+		//float distance = length(firefly0PositionWorld - vertexPositionWorld);
+		float attenuation = 0.3;
 
 
-	//float distance = length(firefly0PositionWorld - vertexPositionWorld);
-	float attenuation = 0.3;
+		vec3 lightVectorWorld = normalize(lightPositionWorld - vertexPositionWorld);
+		//float brightness = max(dot(lightVectorWorld, normal), 0.0);
+		float brightness = max(dot(lightVectorWorld, normalize(normalWorld)), 0.0);
+		vec3 diffuseLight = vec3(brightness, brightness, brightness);
+		diffuseLight = clamp(vec3(attenuation, attenuation, attenuation) * diffuseStrength * diffuseLight, 0, 1);
 
+		//specular light
+		vec3 reflectedLightVectorWorld = reflect(-lightVectorWorld, normalWorld);
+		vec3 eyeVectorWorld = normalize(eyePositionWorld - vertexPositionWorld);
+		float s = clamp(dot(reflectedLightVectorWorld, eyeVectorWorld), 0, 1);
+		s = pow(s, 20);
+		vec3 specularLight = vec3(s, s, s);
+		specularLight = vec3(attenuation, attenuation, attenuation) * specularStrength * specularLight;
 
-	vec3 lightVectorWorld = normalize(lightPositionWorld - vertexPositionWorld);
-	//float brightness = max(dot(lightVectorWorld, normal), 0.0);
-	float brightness = max(dot(lightVectorWorld, normalize(normalWorld)), 0.0);
-	vec3 diffuseLight = vec3(brightness, brightness, brightness);
-	diffuseLight = clamp(vec3(attenuation, attenuation, attenuation) * diffuseStrength * diffuseLight, 0, 1);
-
-	//specular light
-	vec3 reflectedLightVectorWorld = reflect(-lightVectorWorld, normalWorld);
-	vec3 eyeVectorWorld = normalize(eyePositionWorld - vertexPositionWorld);
-	float s = clamp(dot(reflectedLightVectorWorld, eyeVectorWorld), 0, 1);
-	s = pow(s, 10);
-	vec3 specularLight = vec3(s, s, s);
-	specularLight = vec3(attenuation, attenuation, attenuation) * specularStrength*specularLight;
-
-	//ambientLight  = 0 * ambientLight;
-	//ambientLight  = vec3(attenuation , attenuation, attenuation) * ambientLight;
-	//diffuseLight = vec3(attenuation, attenuation, attenuation) *attenuation;
-	//specularLight = vec3(attenuation, attenuation, attenuation) *attenuation;
-
-	daColor = vec4((texture(texture0, UV).rgb + normal)*(ambientLight + diffuseLight + specularLight), 1.0);
-
+		daColor = vec4((texture(texture0, UV).rgb)*(ambientLight + diffuseLight + specularLight), 1.0);
+	}
 }
 
 
